77cd55ffdb2d2ae4e907170664312efaa200be45
WCG847 <heelmods@gmail.com>
2025-05-29 18:34:49 +0100

    Refactor: Remove legacy components and simplify codebase
    Significant restructuring of the codebase:
    - Removed `CBootUpLoader`, `CData`, `CTim2`, and `InitDevice` classes, along with their methods and functionality.
    - Updated `Yukes.pyproj` to exclude deprecated files.
    - Refactored `cfile.py` to introduce new bytearray attributes for memory/file handling.
    - Simplified imports across multiple files by removing unused dependencies.

--- Yukes/InitDevice.py
+++
@@ -1,5 +0,0 @@
-from cfile import CFile
-
-def InitDevice():
-	mem = CFile().Start(CFile.eDevice.HOST)
-	return mem
--- Yukes/cfile.py
+++ Yukes/cfile.py
@@ -1,26 +1,9 @@
-from curses import nocbreak
-from struct import unpack, pack
 from mmap import mmap
-from enum import Enum
+from struct import unpack, pack
 
 class CFile:
-	class eDevice(Enum):
-		HOST = 0
-		CD = 1
-
-	def Start(self, device=eDevice.HOST):
-		self.mem = mmap(-1, 4096)
-		i = 0
-		j = 0
-		while i < 512:
-			self.mem.seek(j)
-			i += 8
-			for _ in range(0x14, 0xF5, 0x20):
-				self.mem.write(pack('I', 7))
-				j += 256
-				if i < 512:
-					continue
-				else:
-					return self.mem
-
+	m_nSector = bytearray(512)
+	m_CashEnt = bytearray(10240)
+	m_FilePos = bytearray(16)
+	m_HeadBuf = bytearray(65536)
 
